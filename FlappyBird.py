1e518e8dfc9c67159c24492fd2535b9197976b2e
Andre Sampaio <89166187+detauportuno@users.noreply.github.com>
2023-09-05 16:44:34 -0300

    Add files via upload
    Mudanças: fundo para o modo cyber será criado, serão utilizadas imagens com a mesma dimensão daquela pedida
    Serão adicionadas musicas para o jogo, utilizando o mixer (nao sei mexer ainda) 
    A musica inicia a partir do momento que o jogador iniciar o jogo com o clique do botão.

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,7 +1,26 @@
 import pygame
 import os
 import random
+from pygame import mixer 
 
+mixer.init() 
+mixer.music.load("song.mp3") 
+mixer.music.set_volume(0.7) 
+mixer.music.play() 
+while True: 
+      
+    print("Press 'p' to pause, 'r' to resume") 
+    print("Press 'e' to exit the program") 
+    query = input("  ") 
+      
+    if query == 'p': 
+        mixer.music.pause()      
+    elif query == 'r': 
+        mixer.music.unpause() 
+    elif query == 'e': 
+        mixer.music.stop() 
+        break
+    
 TELA_LARGURA = 550
 TELA_ALTURA = 800
 
